b5689e9184f374478b08325301892297e75fc650
martsDev <martssilvapro@gmail.com>
2022-10-06 13:21:45 -0300

    add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+       <h1>Slider</h1>
+        
+    </div>
+  );
+};
+
+export default App;
