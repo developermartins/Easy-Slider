ae3a188f9dcd10acafdcf09987764d05a717c7b7
martsDev <martssilvapro@gmail.com>
2022-10-06 15:09:31 -0300

    refactor app

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
-
+import React from 'react';
 import './App.css';
-import images from './img';
+import Slider from './components/Slider';
 
 const App = () => {
-
-  const [width, setWidth] = useState(0);
-  const slider = useRef();
-
-  useEffect(() => {
-    setWidth(slider.current.scrollWidth - slider.current.offsetWidth);
-  }, []);
 
   return (
     <div className="app">
        <h1>Slider</h1>
-        <motion.div ref={ slider } className='slider' whileTap={{ cursor: "grabbing" }}>
-          <motion.div drag='x' dragConstraints={{ right: 0, left: -width }} className='inner-slider'>
-            { images.map((image) => (
-              <motion.div className='slider-item' key={ image }>
-                <img src={ image } alt="image" />
-              </motion.div>
-            )) }
-          </motion.div>
-        </motion.div>
+        <Slider />
     </div>
   );
 };
---
+++ src/components/Slider.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+
+import './Slider.css';
+import images from '../img';
+
+const Slider = () => {
+
+    const [width, setWidth] = useState(0);
+    const slider = useRef();
+  
+    useEffect(() => {
+      setWidth(slider.current.scrollWidth - slider.current.offsetWidth);
+    }, []);
+  
+
+  return (
+    <motion.div ref={ slider } className='slider' whileTap={{ cursor: "grabbing" }}>
+          <motion.div drag='x' dragConstraints={{ right: 0, left: -width }} className='inner-slider'>
+            { images.map((image) => (
+              <motion.div className='slider-item' key={ image }>
+                <img src={ image } alt="image" />
+              </motion.div>
+            )) }
+          </motion.div>
+        </motion.div>
+  );
+};
+
+export default Slider;
