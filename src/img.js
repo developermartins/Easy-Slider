7b1d6d7fe85965177c0783a63d7f11367003d042
martsDev <martssilvapro@gmail.com>
2022-10-06 13:27:51 -0300

    implemets images array

---
+++ src/img.js
@@ -0,0 +1,8 @@
+import img1 from './assets/img1';
+import img2 from './assets/img2';
+import img3 from './assets/img3';
+import img4 from './assets/img4';
+import img5 from './assets/img5';
+import img6 from './assets/img6';
+
+export default [img1, img2, img3, img4, img5, img6];
