8f42fd1697a019cd03e556ecce726447142865a2
martsDev <martssilvapro@gmail.com>
2022-10-06 13:33:39 -0300

    fix images name

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 
 import './App.css';
+import images from './img';
 
 const App = () => {
   return (
@@ -9,7 +10,11 @@ const App = () => {
        <h1>Slider</h1>
         <motion.div className='slider'>
           <motion.div className='inner-slider'>
-            
+            { images.map((image) => (
+              <motion.div className='slider-item'>
+                <img src={ image } alt="image" />
+              </motion.div>
+            )) }
           </motion.div>
         </motion.div>
     </div>
