f18c057fe573576db497e8b614e601ea0d6d8b89
martsDev <martssilvapro@gmail.com>
2022-10-06 14:10:37 -0300

    implements slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import './App.css';
 import images from './img';
 
 const App = () => {
+
+  const [width, setWidth] = useState(0);
+  const slider = useRef();
+
+  useEffect(() => {
+    setWidth(slider.current.scrollWidth - slider.current.offsetWidth);
+  }, []);
+
   return (
     <div className="app">
        <h1>Slider</h1>
-        <motion.div className='slider'>
-          <motion.div className='inner-slider'>
+        <motion.div ref={ slider } className='slider' whileTap={{ cursor: "grabbing" }}>
+          <motion.div drag='x' dragConstraints={{ right: 0, left: -width }} className='inner-slider'>
             { images.map((image) => (
-              <motion.div className='slider-item'>
+              <motion.div className='slider-item' key={ image }>
                 <img src={ image } alt="image" />
               </motion.div>
             )) }
